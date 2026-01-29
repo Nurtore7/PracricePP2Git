1cefc3c4e876b062fdc1803245e3d57a0517db1d
Nurtore Suiinbay <nurtore.suinbay@icloud.com>
2026-01-29 22:12:08 +0500

    Added Variables file

---
+++ variables.py
@@ -0,0 +1,25 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       
+x = "Sally" 
+print(x)
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "awesome"
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+myfunc()
+print("Python is " + x)
