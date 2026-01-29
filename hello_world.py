9d5bbb6a4799a0aaf8b739b1078ec0b977ea762c
Nurtore Suiinbay <nurtore.suinbay@icloud.com>
2026-01-29 22:07:46 +0500

    Some updata in codes

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,13 @@ print ("Hello")
 
 print(2+2)
 print("Some text here")
+
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+print(3)
+print(358)
+print(50000)
+
+print("I am", 35, "years old.")
