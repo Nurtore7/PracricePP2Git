a3138ab74e763b8a66a890304935cab7c4c931fa
Nurtore Suiinbay <nurtore.suinbay@icloud.com>
2026-01-29 22:19:54 +0500

    Added Numbers file

---
+++ numbers.py
@@ -0,0 +1,17 @@
+print(3)
+print(358)
+print(50000)
+
+print(3 + 3)
+print(2 * 5)
+
+print("I am", 35, "years old.")
+
+x="He is"
+print(x, 17)
+
+a = 10
+b = 4
+
+result = a * b + (a - b)
+print(result)
